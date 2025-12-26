41928ef41bb3a72e0a6300a04c8d6c1c44c8768f
Aleem khan <aleem@Aleem.Cricket.computer>
2025-12-26 08:38:14 +0500

    second commit

--- database_utility/storage/local.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-def save_locally(file_path):
-    os.makedirs("backups",exist_ok=True)
-    destination=os.path.join("backups",os.path.basename(file_path))
-    shutil.move(file_path,destination)
-
-    return destination
